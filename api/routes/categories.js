0e83f5d9cbe13aeb9cf291193ed24f82b740b945
Whoisloyka <whoisloyka@gmail.com>
2022-07-27 11:34:05 +0300

    Second part is done. Continue with fetch data

---
+++ api/routes/categories.js
@@ -0,0 +1,17 @@
+const router = require("express").Router();
+const Category = require("../models/Category");
+
+// CREATE CATEGORY
+
+router.post("/", async (req, res) => {
+  try {
+    const cats = await Category.find();
+    res.status(200).json(cats);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// GET CATEGORY
+
+module.exports = router;
